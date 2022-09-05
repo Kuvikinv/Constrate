67b0342e71ce622ef70451dc062a44fb36719e50
Kuvikinv <kuvikinvova@gmail.com>
2022-09-05 15:15:23 +0700

    Initial Commit

---
+++ Constrate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constrate
+//
+//  Created by Admin on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
