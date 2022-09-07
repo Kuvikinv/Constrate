cba2b01b5df03cbee91fdbfed712e7c966369292
Kuvikinv <kuvikinvova@gmail.com>
2022-09-07 21:17:00 +0700

    Ok

--- Constrate/ViewController.swift
+++ Constrate/ViewController.swift
@@ -15,16 +15,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         greetingLabel.isHidden = true
-        greetingButton.layer.cornerRadius = 10
+//        greetingButton.layer.cornerRadius = 10
+        greetingButton.configuration = setupButton(with: "Show Greeting")
     }
 
     @IBAction func greetingButtonPressed() {
         greetingLabel.isHidden.toggle()
         
-        greetingButton.setTitle(
-            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
-            for: .normal
-        )
+        greetingButton.configuration = setupButton(with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting")
+        
+//        greetingButton.setTitle(
+//            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+//            for: .normal
+//        )
         
 //        if greetingLabel.isHidden {
 //            greetingButton.setTitle("Show greeting", for: .normal)
@@ -33,5 +36,14 @@ class ViewController: UIViewController {
 //        }
     }
     
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfuguration = UIButton.Configuration.filled()
+        buttonConfuguration.baseBackgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+        buttonConfuguration.title = title
+        buttonConfuguration.buttonSize = .large
+        buttonConfuguration.cornerStyle = .large
+        buttonConfuguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        return buttonConfuguration
+    }
 }
 
